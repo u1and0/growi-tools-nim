0557ca95792d37af37c89346315df25d33160796
u1and0 <e01.ando60@gmail.com>
2022-06-24 15:41:16 +0900

    [add] JSON property access

--- growi.nim
+++ growi.nim
@@ -1,6 +1,7 @@
 import std/uri
 import std/os
 import std/httpclient
+import std/json
 const ACCESS_TOKEN = getEnv("GROWI_ACCESS_TOKEN") # Get token from https://demo.growi.org/me
 const URL = getEnv("GROWI_URL") # https://demo.growi.org/
 
@@ -15,6 +16,24 @@ const URL = getEnv("GROWI_URL") # https://demo.growi.org/
   #   result.path:string = path
   #   result._json = result.get()
 
+type Revision = object
+  body: string
+  pageId: string
+
+type Creator = object
+  name: string
+  username: string
+  status: int
+
+type Page = object
+  id: string
+  path: string
+  revision: Revision
+  creator: Creator
+
+type Data = object
+  page: Page
+
 proc get(path: string): Response =
   var client = newHttpClient()
   client.headers = newHttpHeaders({"Content-Type": "application/json"})
@@ -28,4 +47,12 @@ proc get(path: string): Response =
 
   return res
 
-echo get("/Wiki選定").body
+let res = get("/社内標準")
+let js = parseJson(res.body)
+let prt = js.pretty()
+echo prt
+# echo js["page"]["revision"]["body"]
+var data = js.to(Data)
+echo data.page.id
+echo data.page.revision
+echo data.page.revision.body

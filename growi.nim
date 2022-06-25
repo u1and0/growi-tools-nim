0a2de77533b701b0e2b4ee71e18c1d844135a7c3
u1and0 <e01.ando60@gmail.com>
2022-06-26 01:22:55 +0900

    [mod] field json _id replace id

--- growi.nim
+++ growi.nim
@@ -2,6 +2,7 @@ import std/uri
 import std/os
 import std/httpclient
 import std/json
+import strutils
 const ACCESS_TOKEN = getEnv("GROWI_ACCESS_TOKEN") # Get token from https://demo.growi.org/me
 const URL = getEnv("GROWI_URL") # https://demo.growi.org/
 
@@ -17,6 +18,7 @@ const URL = getEnv("GROWI_URL") # https://demo.growi.org/
   #   result._json = result.get()
 
 type Revision = object
+  id: string
   body: string
   pageId: string
 
@@ -44,15 +46,15 @@ proc get(path: string): Response =
     "path": path,
   }
   let res = client.get(url ? q)
-
   return res
 
-let res = get("/社内標準")
-let js = parseJson(res.body)
-let prt = js.pretty()
-echo prt
-# echo js["page"]["revision"]["body"]
-var data = js.to(Data)
-echo data.page.id
-echo data.page.revision
-echo data.page.revision.body
+if is_main_module:
+  let res = get(paramStr(1))
+  # underscoreをobjectのfield名にできない仕様のせいで
+  # stringを一部underscoreなしにする
+  let jstr = res.body.multiReplace(
+    ("\"_id\":", "\"id\":")
+  )
+  var data = to(parseJson(jstr), Data)
+  echo data.page.revision.body
+  echo data.page.revision.id

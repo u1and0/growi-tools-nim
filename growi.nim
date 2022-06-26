d4d125ffebbb1e1a997f3d9de6cb744d9f36e295
u1and0 <e01.ando60@gmail.com>
2022-06-26 12:12:13 +0900

    [mod] Data object init

--- growi.nim
+++ growi.nim
@@ -6,35 +6,25 @@ import strutils
 const ACCESS_TOKEN = getEnv("GROWI_ACCESS_TOKEN") # Get token from https://demo.growi.org/me
 const URL = getEnv("GROWI_URL") # https://demo.growi.org/
 
-  # type Page = object
-  #   path: string
-  #   exist: bool
-  #   id : string
-  #   body: string
-  #   json
-  #
-  # proc initPage(path:string):Page =
-  #   result.path:string = path
-  #   result._json = result.get()
+type
+  Revision = object
+    id: string
+    body: string
+    pageId: string
 
-type Revision = object
-  id: string
-  body: string
-  pageId: string
+  Creator = object
+    name: string
+    username: string
+    status: int
 
-type Creator = object
-  name: string
-  username: string
-  status: int
+  Page = object
+    id: string
+    path: string
+    revision: Revision
+    creator: Creator
 
-type Page = object
-  id: string
-  path: string
-  revision: Revision
-  creator: Creator
-
-type Data = object
-  page: Page
+  Data = object
+    page: Page
 
 proc get(path: string): Response =
   var client = newHttpClient()
@@ -48,13 +38,16 @@ proc get(path: string): Response =
   let res = client.get(url ? q)
   return res
 
-if is_main_module:
-  let res = get(paramStr(1))
+proc initData(path: string): Data =
+  let res: Response = get(path)
   # underscoreをobjectのfield名にできない仕様のせいで
   # stringを一部underscoreなしにする
-  let jstr = res.body.multiReplace(
+  let jsonStr = res.body.multiReplace(
     ("\"_id\":", "\"id\":")
   )
-  var data = to(parseJson(jstr), Data)
+  to(parseJson(jsonStr), Data)
+
+if is_main_module:
+  let data = initData(paramStr(1))
   echo data.page.revision.body
   echo data.page.revision.id

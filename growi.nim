3183fa1190ad06a06a02f58a3d8893147c66d95e
u1and0 <e01.ando60@gmail.com>
2022-06-26 21:32:16 +0900

    [add] flag for page exist or not

--- growi.nim
+++ growi.nim
@@ -1,10 +1,13 @@
+## Growi API
 import std/uri
 import std/os
 import std/httpclient
 import std/json
 import strutils
-const ACCESS_TOKEN = getEnv("GROWI_ACCESS_TOKEN") # Get token from https://demo.growi.org/me
-const URL = getEnv("GROWI_URL") # https://demo.growi.org/
+## Get token from https://demo.growi.org/me
+const ACCESS_TOKEN = getEnv("GROWI_ACCESS_TOKEN")
+## https://demo.growi.org/
+const URL = getEnv("GROWI_URL")
 
 type
   Revision = object
@@ -25,29 +28,33 @@ type
 
   Data = object
     page: Page
+    exist: bool
 
 proc get(path: string): Response =
   var client = newHttpClient()
   client.headers = newHttpHeaders({"Content-Type": "application/json"})
   var url = parseUri(URL)
   url.path = "_api/v3/page"
-  let q = {
-    "access_token": ACCESS_TOKEN,
-    "path": path,
-  }
+  let q = {"access_token": ACCESS_TOKEN, "path": path}
   let res = client.get(url ? q)
   return res
 
 proc initData(path: string): Data =
   let res: Response = get(path)
+  if res.status != $Http200:
+    result.exist = false
+    return
   # underscoreをobjectのfield名にできない仕様のせいで
   # stringを一部underscoreなしにする
   let jsonStr = res.body.multiReplace(
     ("\"_id\":", "\"id\":")
   )
-  to(parseJson(jsonStr), Data)
+  result.page = to(parseJson(jsonStr)["page"], Page)
+  result.exist = true
 
 if is_main_module:
   let data = initData(paramStr(1))
-  echo data.page.revision.body
-  echo data.page.revision.id
+  if data.exist:
+    echo data.page.revision.body
+    echo data.page.revision.id
+  else: echo "Page not exist"
